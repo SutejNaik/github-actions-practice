b71bd683dca9f90b581acabd2c4271199f37e1cf
SutejNaik <sutejnaik716@gmail.com>
2026-09-16 16:42:51 +0000

    Install Flask before tests

--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
-from app import add
+from app import app
 
-assert add(2, 3) == 5
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hello from GitHub Actions -> ECR -> EC2!"
 
 print("Test passed!")
 
